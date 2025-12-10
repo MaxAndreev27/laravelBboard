92d456c940c715afb566d9410dbb230e9f726d55
max <maxandreev27@gmail.com>
2025-12-10 21:55:10 +0200

    Add ddev config for local run with Docker

--- database/seeders/BbSeeder.php
+++ database/seeders/BbSeeder.php
@@ -14,14 +14,14 @@ class BbSeeder extends Seeder
      */
     public function run(): void
     {
-        DB::table('Bbs')->insert(['title'=>'Холодильник', 'content'=>'Новий холодильник', 'price'=>2000, 'user_id'=>1]);
-        DB::table('Bbs')->insert(['title'=>'Телевізор', 'content'=>'Телевізор Panasonic', 'price'=>4000, 'user_id'=>1]);
-        DB::table('Bbs')->insert(['title'=>'Пилесос', 'content'=>'Пилесос Roventa', 'price'=>3000, 'user_id'=>1]);
-        DB::table('Bbs')->insert(['title'=>'Шафа', 'content'=>'Шафа платяна', 'price'=>1000, 'user_id'=>2]);
-        DB::table('Bbs')->insert(['title'=>'Кіт', 'content'=>'Старий кіт', 'price'=>5000, 'user_id'=>2]);
-        DB::table('Bbs')->insert(['title'=>'Сірники', 'content'=>'Коробка сірників', 'price'=>50, 'user_id'=>2]);
-        DB::table('Bbs')->insert(['title'=>'Стіл', 'content'=>'Новий стіл', 'price'=>500, 'user_id'=>3]);
-        DB::table('Bbs')->insert(['title'=>'Стул', 'content'=>'Новий стул', 'price'=>350, 'user_id'=>3]);
+        DB::table('bbs')->insert(['title'=>'Холодильник', 'content'=>'Новий холодильник', 'price'=>2000, 'user_id'=>1]);
+        DB::table('bbs')->insert(['title'=>'Телевізор', 'content'=>'Телевізор Panasonic', 'price'=>4000, 'user_id'=>1]);
+        DB::table('bbs')->insert(['title'=>'Пилесос', 'content'=>'Пилесос Roventa', 'price'=>3000, 'user_id'=>1]);
+        DB::table('bbs')->insert(['title'=>'Шафа', 'content'=>'Шафа платяна', 'price'=>1000, 'user_id'=>2]);
+        DB::table('bbs')->insert(['title'=>'Кіт', 'content'=>'Старий кіт', 'price'=>5000, 'user_id'=>2]);
+        DB::table('bbs')->insert(['title'=>'Сірники', 'content'=>'Коробка сірників', 'price'=>50, 'user_id'=>2]);
+        DB::table('bbs')->insert(['title'=>'Стіл', 'content'=>'Новий стіл', 'price'=>500, 'user_id'=>3]);
+        DB::table('bbs')->insert(['title'=>'Стул', 'content'=>'Новий стул', 'price'=>350, 'user_id'=>3]);
 
 //        Bb::create(['title'=>'Холодильник', 'content'=>'Новий холодильник', 'price'=>2000, 'user_id'=>1]);
 //        Bb::create(['title'=>'Телевізор', 'content'=>'Телевізор Panasonic', 'price'=>4000, 'user_id'=>1]);
